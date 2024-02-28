55430ada8c81d16f0b53c6901d6b5f95a7851a07
marco <marco@joinder.me>
2024-02-27 22:07:29 -0300

    Initial commit

---
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/UseCaseTest.java
@@ -0,0 +1,15 @@
+package com.fullcycle.admin.catalogo.application;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UseCaseTest {
+
+
+    @Test
+    public void testExecute() {
+        UseCase useCase = new UseCase();
+        Assertions.assertNotNull(useCase);
+        Assertions.assertNotNull(useCase.execute());
+    }
+}
