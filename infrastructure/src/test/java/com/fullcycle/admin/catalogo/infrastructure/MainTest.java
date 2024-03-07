99a043afeaa78f8e5a69f3480788faf32936fff8
marco <marco@joinder.me>
2024-03-07 09:56:51 -0300

    Configured Spring infra

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/MainTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/MainTest.java
@@ -1,14 +1,17 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
+import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 public class MainTest {
 
     @Test
     public void testMain() {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
         Assertions.assertNotNull(new Main());
-        Main.main(new String[] {});
-
+        Main.main(new String[]{});
     }
 }
