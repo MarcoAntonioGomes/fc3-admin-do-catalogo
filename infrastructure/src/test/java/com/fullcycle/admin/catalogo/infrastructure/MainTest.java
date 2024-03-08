0118212902763c3ffdf41176c92f1a524db3c0e9
marco <marco@joinder.me>
2024-03-07 22:21:04 -0300

    Developed CategoryRepository and configured MSQLGatewayTest

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/MainTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/MainTest.java
@@ -1,10 +1,17 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJPAEntity;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
+
+import java.util.List;
 
 public class MainTest {
 
@@ -14,4 +21,7 @@ public class MainTest {
         Assertions.assertNotNull(new Main());
         Main.main(new String[]{});
     }
+
+
+
 }
---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -0,0 +1,28 @@
+package com.fullcycle.admin.catalogo.infrastructure.category;
+
+import com.fullcycle.admin.catalogo.infrastructure.MySQLGatewayTest;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@MySQLGatewayTest
+public class CategoryMySQLGatewayTest {
+
+    @Autowired
+    private CategoryMySQLGateway categoryMySQLGateway;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+
+    @Test
+    public void testInjectedDependencies() {
+        Assertions.assertNotNull(categoryMySQLGateway);
+        Assertions.assertNotNull(categoryRepository);
+    }
+
+
+
+}
