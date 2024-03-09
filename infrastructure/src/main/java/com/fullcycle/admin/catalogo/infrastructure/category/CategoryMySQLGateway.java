cda3f4dd2d05f2ba104c93264316f2b0624f361f
marco <marco@joinder.me>
2024-03-09 16:49:46 -0300

    Unit Tests CategoryMySQLGateway

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
@@ -6,43 +6,79 @@ import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
 import com.fullcycle.admin.catalogo.domain.category.CategoryID;
 import com.fullcycle.admin.catalogo.domain.category.CategorySearchQuery;
 import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJPAEntity;
 import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import com.fullcycle.admin.catalogo.infrastructure.utils.SpecificationUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.*;
 import java.util.Optional;
+
+import static com.fullcycle.admin.catalogo.infrastructure.utils.SpecificationUtils.like;
 
 @Service
 public class CategoryMySQLGateway implements CategoryGateway {
 
-    private final CategoryRepository categoryRepository;
+    private final CategoryRepository repository;
 
     public CategoryMySQLGateway(final CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
+        this.repository = categoryRepository;
     }
 
     @Override
-    public Category create(Category aCategory) {
-        return null;
+    public Category create(final Category aCategory) {
+        return this.repository.save(CategoryJPAEntity.from(aCategory)).toAggregate();
     }
 
     @Override
     public void deleteById(CategoryID anId) {
-
+        if (this.repository.existsById(anId.getValue())) {
+            this.repository.deleteById(anId.getValue());
+        }
     }
 
     @Override
-    public Optional<Category> findById(CategoryID anId) {
-        return Optional.empty();
+    public Optional<Category> findById(final CategoryID anId) {
+        return this.repository.findById(anId.getValue()).map(CategoryJPAEntity::toAggregate);
     }
 
     @Override
-    public Category update(Category aCategory) {
-        return null;
+    public Category update(final Category aCategory) {
+        return this.repository.save(CategoryJPAEntity.from(aCategory)).toAggregate();
     }
 
     @Override
-    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
-        return null;
+    public Pagination<Category> findAll(final CategorySearchQuery aQuery) {
+
+
+        final var page = PageRequest.of(
+                aQuery.page(),
+                aQuery.perPage(),
+                Sort.by(Sort.Direction.fromString(aQuery.direction()), aQuery.sort())
+        );
+
+        final var specification = Optional.ofNullable(aQuery.terms())
+                .filter(str -> !str.isBlank())
+                .map (str ->
+                    SpecificationUtils
+                           .<CategoryJPAEntity>like("name", str)
+                            .or(like("description", str))
+
+                ).orElse(null);
+
+
+        var pageResult = this.repository.findAll(Specification.where(specification), page);
+
+
+        return new Pagination<>(
+                pageResult.getNumber(),
+                pageResult.getSize(),
+                pageResult.getTotalElements(),
+                pageResult.map(CategoryJPAEntity::toAggregate).toList()
+        );
     }
 }
---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/category/persistence/CategoryRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.fullcycle.admin.catalogo.infrastructure.category.persistence;
+
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.infrastructure.MySQLGatewayTest;
+import org.hibernate.PropertyValueException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+
+
+
+@MySQLGatewayTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void givenAnInvalidNullName_whenCallsSave_shouldReturnError() {
+       final var aCategory = Category.newCategory("Filmes", "Filmes de todos os tipos", true);
+       final var expectedProperty = "name";
+       final var expectedMessage = "not-null property references a null or transient value : com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJPAEntity.name";
+
+       final var anEntity = CategoryJPAEntity.from(aCategory);
+       anEntity.setName(null);
+
+       final var actualException = Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(anEntity));
+       final var actualCause = Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+       Assertions.assertEquals( expectedProperty, actualCause.getPropertyName());
+       Assertions.assertEquals(expectedMessage, actualException.getCause().getMessage());
+
+    }
+
+    @Test
+    public void givenAnInvalidNullCreatedAt_whenCallsSave_shouldReturnError() {
+        final var aCategory = Category.newCategory("Filmes", "Filmes de todos os tipos", true);
+        final var expectedProperty = "createdAt";
+        final var expectedMessage = "not-null property references a null or transient value : com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJPAEntity.createdAt";
+
+        final var anEntity = CategoryJPAEntity.from(aCategory);
+        anEntity.setCreatedAt(null);
+
+        final var actualException = Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(anEntity));
+        final var actualCause = Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals( expectedProperty, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedMessage, actualException.getCause().getMessage());
+
+    }
+
+    @Test
+    public void givenAnInvalidNullUpdatedAt_whenCallsSave_shouldReturnError() {
+        final var aCategory = Category.newCategory("Filmes", "Filmes de todos os tipos", true);
+        final var expectedProperty = "updatedAt";
+        final var expectedMessage = "not-null property references a null or transient value : com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJPAEntity.updatedAt";
+
+        final var anEntity = CategoryJPAEntity.from(aCategory);
+        anEntity.setUpdatedAt(null);
+
+        final var actualException = Assertions.assertThrows(DataIntegrityViolationException.class, () -> categoryRepository.save(anEntity));
+        final var actualCause = Assertions.assertInstanceOf(PropertyValueException.class, actualException.getCause());
+
+        Assertions.assertEquals( expectedProperty, actualCause.getPropertyName());
+        Assertions.assertEquals(expectedMessage, actualException.getCause().getMessage());
+
+    }
+
+
+}
